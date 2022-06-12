61a0336eb9250413c7c2e9223ae336b2a2e69757
Joseph Senecal <jasenecal2@gmail.com>
2022-06-12 16:57:52 -0600

    Test: begin response shape tests

--- spec/requests/api/v1/forcast_endpoint_spec.rb
+++ spec/requests/api/v1/forcast_endpoint_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Retrieve weather for a city endpoint: " do
 
   it "is currently responsive" do
-    get "/api/v1/forecast?location=denver,co"
+    get "/api/v1/forecast?location=denver,co", params: { location: "denver,co" }, headers: { "Accept" => "application/json", "Content-Type" => "application/json" }
     expect(response.status.to_s).to match /2\d\d/
   end
 
@@ -27,6 +27,11 @@ RSpec.describe "Retrieve weather for a city endpoint: " do
 
   context "Responds with the correct JSON shape: ", :vcr do
 
+    it "contains the correct headers nested in the correct order" do
+      get "/api/v1/forecast", params: { location: "denver,co" }, headers: { "Accept" => "application/json", "Content-Type" => "application/json" }
+      response_body = JSON.parse response.body, symbolize_names: true
+      expect(response_body.keys).to eq [:data]
+    end
   end
 
   context "Edge Cases and Unexpected Use Cases: ", :vcr do
