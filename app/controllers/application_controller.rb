48b4f933523ad17d337693205b0ffeada3c5415e
Joseph Senecal <jasenecal2@gmail.com>
2022-06-12 16:57:00 -0600

    Feat: application controller method #check_headers checks all incoming requests for common header requirements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+
+  def check_header
+    if request.headers["Content-Type"] != "application/json" || request.headers["Accept"] != "application/json"
+      render json: { "error": "request header should specify {'Content-Type' => 'application/json', 'Accept' => 'application.json'}" }, status: 406
+    end
+  end
 end
