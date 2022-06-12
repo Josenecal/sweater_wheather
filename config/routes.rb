3e5dc270a45936d56f7378e19d4299040f127cad
Joseph Senecal <jasenecal2@gmail.com>
2022-06-12 16:27:46 -0600

    Ref: namespace forecast controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'api/v1/forecast', to: 'forecasts#show'
+  get 'api/v1/forecast', to: 'api/v1/forecasts#show', status: 200
 
 end
