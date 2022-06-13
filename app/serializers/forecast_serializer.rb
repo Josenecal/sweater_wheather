965454e4b7b84757d27002d1a5febc39030c27cb
Joseph Senecal <jasenecal2@gmail.com>
2022-06-13 00:24:36 -0600

    Setup: forecast serializer

---
+++ app/serializers/forecast_serializer.rb
@@ -0,0 +1,28 @@
+class ForecastSerializer
+
+  def self.response(forecast)
+    {
+      "data": {
+        "id": nil,
+        "type": "forecast",
+        "attributes": {
+          "current_weather": {
+            "datetime": forecast.current_weather[:datetime],
+            "temperature": forecast.current_weather[:temp],
+            "sunrise": forecast.current_weather[:sunrise],
+            "sunset": forecast.current_weather[:sunset],
+            "feels_like": forecast.current_weather[:feels_like],
+            "humidity": forecast.current_weather[:humidity],
+            "uvi": forecast.current_weather[:uvi],
+            "visibility": forecast.current_weather[:visibility],
+            "conditions": forecast.current_weather[:conditions],
+            "icon": forecast.current_weather[:icon]
+          },
+          "daily_weather": forecast.daily_weather,
+          "hourly_weather": forecast.hourly_weather
+        }
+      }
+    }
+  end
+
+end
