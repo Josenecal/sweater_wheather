53b3e275f896c2628f68be0bdee2fbdaddd112ac
Joseph Senecal <jasenecal2@gmail.com>
2022-06-12 20:17:38 -0600

    Ref: update name of connection method and parse response

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -1,11 +1,11 @@
 class MapquestService
 
-  def self.mapquest
+  def self.mapquest_conn
     connection = Faraday.new(url: "http://www.mapquestapi.com", params: { key: ENV['mapquest_api_key'] })
   end
 
   def self.geocode_address(location)
-    response = mapquest.get("/geocoding/v1/address", { location: location })
+    response = mapquest_conn.get("/geocoding/v1/address", { location: location })
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
 
