0ddbbde81109539b4f2a5c1aef268b334821e8a6
Joseph Senecal <jasenecal2@gmail.com>
2022-06-13 11:05:42 -0600

    Setup: book poro attributes

---
+++ app/poros/Book.rb
@@ -0,0 +1,11 @@
+class Book
+
+  attr_reader :isbn, :title, :publisher
+
+  def initialize(book_hash)
+    @isbn = book_hash[:isbn]
+    @title = book_hash[:title]
+    @publisher = book_hash[:publisher]
+  end
+
+end
