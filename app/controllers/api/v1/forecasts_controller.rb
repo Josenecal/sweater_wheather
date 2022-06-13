5574287861024f8f5222e2001f41897f8d69ec88
Joseph Senecal <jasenecal2@gmail.com>
2022-06-13 00:27:03 -0600

    Fix: forecasts controller show endpoit

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::ForecastsController < ApplicationController
   before_action :check_header
 
   def show
-    render json: { "message": "under development" }
+    forecast_obj = ForecastFacade.get_forecast_from_location(params[:location])
+    render json: ForecastSerializer(forecast_obj), status: 200
   end
 
 end
