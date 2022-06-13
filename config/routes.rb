9ea7bf3b7b9838ce5d3098417d8b7d68b67e6713
Joseph Senecal <jasenecal2@gmail.com>
2022-06-13 00:28:22 -0600

    Setup: background endpoint scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'api/v1/forecast', to: 'api/v1/forecasts#show', status: 200
+  get 'api/v1/forecast', to: 'api/v1/forecasts#show'
+  get 'api/v1/backgrounds', to: 'api/v1/backgrounds#show'
 
 end
