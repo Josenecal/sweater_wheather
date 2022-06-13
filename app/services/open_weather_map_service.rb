255f5d138a29d49090b9779421797fe09bab9c83
Joseph Senecal <jasenecal2@gmail.com>
2022-06-13 00:25:50 -0600

    Fix: minor tweaks

--- app/services/open_weather_map_service.rb
+++ app/services/open_weather_map_service.rb
@@ -1,10 +1,11 @@
 class OpenWeatherMapService
 
   def self.openweather_conn
-    connection = Faraday.new(:get "https://api.openweathermap.org", params: {appid: ENV['openweather_api_key']})
+    connection = Faraday.new(url: "https://api.openweathermap.org", params: {appid: ENV['openweather_api_key']})
   end
 
-  def self.get_forcast(lat,lng)
-    response = openweather_conn.get("")
-    parsed = JSON.parse(response.body)
+  def self.get_forecast(lat,lng)
+    response = openweather_conn.get("/data/2.5/onecall", {lat: lat, lon: lng, exclude: "minutely,alerts"})
+    parsed = JSON.parse(response.body, symbolize_names: true)
+  end
 end
