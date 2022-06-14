3d7de832665cbf64d51a7b0d1d304b00a25d137b
Joseph Senecal <jasenecal2@gmail.com>
2022-06-14 01:03:19 -0600

    Feat: new users have an assigned API key attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   validates_presence_of :email
   validates_uniqueness_of :email
   validates_presence_of :password_digest
-  validates_presence_of :api_token
-  validates_uniqueness_of :api_token
+  validates_presence_of :api_key
+  validates_uniqueness_of :api_key
 
   has_secure_password
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,11 +10,12 @@ RSpec.describe User do
 
   context 'bcrypt password concealment' do
     it "saves a given password as password digest, which does not match an actual password" do
-      new_user = User.create!(email: "pabu@ferritmail.org", password: "abc123", password_confirmation: "abc123")
+      new_user = User.create!(email: "pabu@ferritmail.org", password: "abc123", password_confirmation: "abc123", api_key: SecureRandom.hex)
       saved_user = User.find(new_user.id)
       expect(saved_user.id.class).to eq Integer
       expect(saved_user.password_digest).not_to eq "abc123"
       expect(saved_user.password).to eq nil
+      expect(saved_user.api_key).not_to eq nil
     end
   end
 
