c1f053c4b47b809756d56998c4d7fed6533dd25f
Joseph Senecal <jasenecal2@gmail.com>
2022-06-13 21:45:10 -0600

    Setup: User model and model tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User do
+
+  it 'validations' do
+    should validate_presence_of :email
+    should validate_uniqueness_of :email
+    should validate_presence_of :password_digest
+  end
+
+  context 'bcrypt password concealment' do
+    it "saves a given password as password digest, which does not match an actual password" do
+      new_user = User.create!(email: "pabu@ferritmail.org", password: "abc123", password_confirmation: "abc123")
+      saved_user = User.find(new_user.id)
+      expect(saved_user.id.class).to eq Integer
+      expect(saved_user.password_digest).not_to eq "abc123"
+      expect(saved_user.password).to eq nil
+    end
+  end
+
+end
