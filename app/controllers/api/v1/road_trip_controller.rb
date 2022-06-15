6738ee0bee01623e72767cd276fc5a401f917ea8
Joseph Senecal <jasenecal2@gmail.com>
2022-06-14 22:04:29 -0600

    Setup: roadtrip controller

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,21 @@
+class RoadTripController < ApplicationController
+
+  before_action :check_headers, :check_api_key
+
+  def create
+    RoadTripFacade.get_roadtrip(roadtrip_params)
+  end
+
+  private
+
+  def roadtrip_params
+    parameters = JSON.parse(request.body)
+    return {
+      start_city: parameters[:start_city],
+      end_city: parameters[:end_city],
+      travel_time: nil,
+      weather_at_eta: nil
+    }
+  end
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::API
       render json: { "error": "request header should specify {'Content-Type' => 'application/json', 'Accept' => 'application.json'}" }, status: 406
     end
   end
+
+  def check_api_key
+    key = JSON.parse(request.body.read, symbolize_names: true)[:api_key]
+    unless User.exists? api_key: key
+      render json: { "error": "unauthorized" }, status: 401
+  end
+
 end
